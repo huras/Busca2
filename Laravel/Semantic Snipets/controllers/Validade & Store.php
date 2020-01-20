e2c4e322ecb12b507f695758e9f34dd922eba10f
huras <hurast@gmail.com>
2020-01-19 22:14:58 -0300

    how to show $val errors + recaptcha validation + model back-end validation + session message return + url validation

--- Laravel/Semantic Snipets/controllers/Validade & Store.php
+++ Laravel/Semantic Snipets/controllers/Validade & Store.php
@@ -9,20 +9,31 @@ use Validator;
 class ClientesController extends Controller
 {
   public function create(Request $request){
+    $form = $request->all();
   
-    $val = $this->validateRequest($request->all());
-    if ($val->fails()) {
-      return redirect()->route('landingpage')
-          ->withErrors($val)
-          ->withInput()
-          ->with([
-            'create-modal-open' => true]);
-    }
+    $val = $this->validateRequest($form);
+    //dd($val->errors());
+    if($form['g-recaptcha-response'] == null){
+          return redirect()->action('TesteDeSiteController@showForm')
+                  ->withInput()
+                  ->withErrors($val)
+                  ->with(['error-recaptcha' => true]);
+      } else if ($val->fails()) {
+          return redirect()->action('TesteDeSiteController@showForm')
+                  ->withInput()
+                  ->withErrors($val)
+                  ->with(['error-recaptcha' => true]);
+      } else {
+
+      }
+    
+      return view('pages/teste-seu-site')->with(['form-success' => true]);;
   }
 
   private function validateRequest($request)
   {
     $rules = [
+        'site' => 'required|url',
         'nome' => 'required',
         'email' => 'required|email',            
         'nascimento' => 'date_format:d/m/Y',
@@ -34,6 +45,7 @@ class ClientesController extends Controller
         'numeric' => 'Este campo só aceita valores numéricos',
         'unique' => 'Cadastro já existente com esta informação',
         'date_format' => 'Data inválida',
+        'url' => 'Endereço inválido',
     ];
 
     return Validator::make($request, $rules, $messages);
